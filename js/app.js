dbc5aecd5b23e17303f5e07cad6826745e4298c0
Keith <kayzee@live.com>
2019-05-02 03:52:35 -0700

    init css

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
 'use strict';
+
+window.onload = function() {
+	console.log(window.geemails);
+	let tbl = document.getElementById('tbl');
+	let email = 0;
+
+	window.geemails.forEach(function(msg) {
+		loadEmail(msg)
+	}); 
+
+	function loadEmail(msg) {
+		email++;
+		// show number of messages
+		document.getElementById('inbox').innerHTML = `Inbox ${email}`;
+		
+		// create row and cell for the email
+		let rows = tbl.insertRow(1);
+		let senderCell = rows.insertCell(0);
+		senderCell.innerHTML = msg.sender;
+		let subjectCell = rows.insertCell(1);
+		subjectCell.innerHTML = msg.subject;
+		let dateCell = rows.insertCell(2);
+		dateCell.innerHTML = msg.date;
+		let body = msg.body;
+
+		// print info and message
+		rows.onclick = function() {
+			document.getElementById('email-info').innerHTML = `${msg.date}<br>${msg.sender}<br>${msg.subject}`;
+			document.getElementById('email-content').innerHTML = `${body}`;
+		}
+	};
+	// render new message
+	setInterval(function() {
+		loadEmail(getNewMessage());
+	}, 8000)
+};
